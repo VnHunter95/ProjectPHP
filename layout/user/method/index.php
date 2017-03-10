5f0df330b3e8a08cdaf054fc0a6901026c26985d
= <tranvytoan@gmail.com>
2017-03-10 11:17:22 +0700

    Move files to correct folder

--- layout/user/method/index.php
+++ layout/user/method/index.php
@@ -1,82 +1,35 @@
-<!-- File Demo Method -->
+<!--File DEMO 3 method-->
 <!DOCTYPE html>
-<html lang="vi">
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <div id="wrapper">
-          <?php
-            include('searchProduct.php');
-            echo "<br><br>";
-            echo "<h1>Demo 1 - Search Theo Tên - searchProduct(1,'Love')";
-            $list = searchProduct(1,'Love');
-            foreach($list as $item)
-            {
-                echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-            }
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 2 - Search Theo Group - searchProduct(2,'Figur')";
-            $list = searchProduct(2,'Figur');
-            foreach($list as $item)
-            {
-                echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-            }
+<html lang='vi'>
+  <head>
+    <title>Demo 3 method</title>
+  </head>
+  <body>
+    <?php
+      include('getProductGroup.php');
+      include('getTag.php');
+      include('getSupplier.php');
 
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 3 - Search Theo Supplier - searchProduct(3,'Good Smi')";
-            $list = searchProduct(3,'Good Smi');
-            if(empty($list))
-            {
-              echo "<h3> NOT FOUND ! </h3>";
-            }else{
-              foreach($list as $item)
-              {
-                  echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-              }
-            }
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 3.5 - Search Theo Supplier - searchProduct(3,'GoodSmi')";
-            $list = searchProduct(3,'GoodSmi');
-            if(empty($list))
-            {
-              echo "<h3> NOT FOUND ! </h3>";
-            }else{
-              foreach($list as $item)
-              {
-                  echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-              }
-            }
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 4 - Search Theo Tag - searchProduct(4,'Sword Art')";
-            $list = searchProduct(4,'Sword Art');
-            foreach($list as $item)
-            {
-                echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-            }
+      $tags = getTag();
+      $productGroups= getProductGroup();
+      $suppliers= getSupplier();
 
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 5 - Search Theo Description - searchProduct(5,'Kiroto và Asuna')";
-            $list = searchProduct(5,'Kiroto và Asuna');
-            foreach($list as $item)
-            {
-                echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-            }
+      echo '<h2> TAGS ! </h2>';
+      foreach($tags as $item)
+      {
+        echo '</br><p>'.$item['tag_name'].'</p>';
+      }
+      echo '</br></br><h2> Groups ! </h2>';
+      foreach($productGroups as $item)
+      {
+        echo '</br><p>'.$item['product_group_name'].'</p>';
+      }
 
-            unset($list);
-            echo "<br><br>";
-            echo "<h1>Demo 6 - Search All - searchProduct(0,'SAO')";
-            $list = searchProduct(0,'SAO');
-            foreach($list as $item)
-            {
-                echo "<h3>".$item['product_id']."</h3><br><h2>".$item['product_name']."</h2>";
-            }
-          ?>
-        </div>
-    </body>
+      echo '</br></br><h2> SUPPLIERs ! </h2>';
+      foreach($suppliers as $item)
+      {
+        echo '</br><p>'.$item['supplier_name'].'</p>';
+      }
+    ?>
+  </body>
 </html>
