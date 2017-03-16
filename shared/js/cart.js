9f312dc074b9ec3c8dcbf0ab71c8922ee6099747
tuananh08 <trantuananhninjapro@yahoo.com.vn>
2017-03-16 08:04:20 -0700

    text cart thu dum tui cai toan
    store_items.php
    style.css
    cart.js

---
+++ shared/js/cart.js
@@ -0,0 +1,55 @@
+function showEditBox(editobj,id) {
+	$('#frmAdd').hide();
+	$(editobj).prop('disabled','true');
+	var currentMessage = $("#message_" + id + " .message-content").html();
+	var editMarkUp = '<textarea rows="5" cols="80" id="txtmessage_'+id+'">'+currentMessage+'</textarea><button name="ok" onClick="callCrudAction(\'edit\','+id+')">Save</button><button name="cancel" onClick="cancelEdit(\''+currentMessage+'\','+id+')">Cancel</button>';
+	$("#message_" + id + " .message-content").html(editMarkUp);
+}
+function cancelEdit(message,id) {
+	$("#message_" + id + " .message-content").html(message);
+	$('#frmAdd').show();
+}
+function cartAction(action,product_code) {
+	var queryString = "";
+	if(action != "") {
+		switch(action) {
+			case "add":
+				queryString = 'action='+action+'&product_id'+ product_code+'&quantity='+$("#qty_"+product_code).val();
+			break;
+			case "remove":
+				queryString = 'action='+action+'&product_id='+ product_code;
+			break;
+			case "empty":
+				queryString = 'action='+action;
+			break;
+		}
+	}
+	jQuery.ajax({
+	url: "ajax_action.php",
+	data:queryString,
+	type: "POST",
+	success:function(data){
+		$("#cart-item").html(data);
+		if(action != "") {
+			switch(action) {
+				case "add":
+					$("#add_"+product_code).hide();
+					$("#added_"+product_code).show();
+				break;
+				case "remove":
+					$("#add_"+product_code).show();
+					$("#added_"+product_code).hide();
+				break;
+				case "empty":
+					$(".btnAddAction").show();
+					$(".btnAdded").hide();
+				break;
+			}
+		}
+	},
+	error:function (){}
+	});
+  $(document).ready(function () {
+	cartAction('','');
+})
+}
