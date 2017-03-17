dca92ef338d64524944c6cfdc185a3dc69a147cf
= <tranvytoan@gmail.com>
2017-03-17 13:33:15 +0700

    Check user Banned or not

--- layout/user/method/login.php
+++ layout/user/method/login.php
@@ -3,14 +3,18 @@
     function login($username,$password)
     {
       $db = new DB();
-      $sql = "SELECT username FROM user WHERE password = '".$password."'";
+      $sql = "SELECT username, is_banned FROM user WHERE username = '".$username."' AND password = '".$password."'";
       $res = $db->select_to_array($sql);
       if(count($res) === 1)
       {
+        if($res[0]['is_banned'] == '1')
+        {
+          return 3;
+        }
         $_SESSION['username'] = $res[0]['username'];
-        return true;
+        return 1;
       }else {
-        return false;
+        return 2;
       }
     }
 ?>
