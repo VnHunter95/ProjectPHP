e17a71ce55b4645278b78a58580f878f9278b827
= <tranvytoan@gmail.com>
2017-03-19 00:25:02 +0700

    done suppiler admin, need to check for product belong to suppiler b4 delete

--- layout/admin/quan-ly-nha-cung-cap.php
+++ layout/admin/quan-ly-nha-cung-cap.php
@@ -1,5 +1,60 @@
 <?php
   include('header.php');
+  require_once($_SERVER['DOCUMENT_ROOT'].'/class/Supplier.Class.php');
+  if(isset($_POST['editSubmit']))
+  {
+    $id       = $_POST['editSubmit'];
+    $name     = $_POST['nameedit'];
+    $phone    = $_POST['phoneedit'];
+    $adr      = $_POST['addressedit'];
+    if(isset($_POST['activeedit']))
+    {
+      $active = '1';
+    }else {
+      $active = '0';
+    }
+    $newSupplier = new Supplier($id,$name,$adr,$phone,$active);
+    $res = $newSupplier->edit();
+    if($res == -1)
+    {
+      echo '<script>alert("Sửa nhà sản xuất thất bại")</script>';
+    }else {
+      echo '<script>alert("Sửa nhà sản xuất thành công")</script>';
+    }
+  }
+  if(isset($_POST['addSubmit']))
+  {
+    $id       = $_POST['addSubmit'];
+    $name     = $_POST['name'];
+    $phone    = $_POST['phone'];
+    $adr      = $_POST['address'];
+    if(isset($_POST['active']))
+    {
+      $active = '1';
+    }else {
+      $active = '0';
+    }
+    $newSupplier = new Supplier(null,$name,$adr,$phone,$active);
+    $res = $newSupplier->save();
+    if($res == -1 || $res == 0)
+    {
+      echo '<script>alert("Thêm nhà sản xuất thất bại")</script>';
+    }else {
+      echo '<script>alert("Thêm nhà sản xuất thành công")</script>';
+    }
+  }
+  if(isset($_POST['deleteSubmit']))
+  {
+    $id = $_POST['deleteSubmit'];
+    $cus = new Supplier($id,'','','','');
+    $res = $cus->delete();
+    if($res == 0 || $res == -1)
+    {
+        echo '<script>alert("Xóa thất bại !")</script>';
+    }else {
+        echo '<script>alert("Xóa thành công !")</script>';
+    }
+  }
 ?>
 	<!--navbar-->
 
@@ -43,27 +98,27 @@
 											<button type="button" class="close" data-dismiss="modal">&times;</button>
 											<h4 class="modal-title">Thêm nhà cung cấp</h4>
 										</div>
-										<form>
+										<form action="quan-ly-nha-cung-cap.php" method="POST">
 											<div class="modal-body">
 												<div class="form-group">
 													<label for="name">Tên nhà cung cấp:</label>
-													<input type="text" class="form-control" id="name">
+													<input type="text" class="form-control" id="name" name="name">
 												</div>
 												<div class="form-group">
 													<label for="address">Địa chỉ:</label>
-													<textarea rows="6" id="address" class="form-control"></textarea>
+													<textarea rows="6" id="address" class="form-control" name="address"></textarea>
 												</div>
 												<div class="form-group">
 													<label for="phone">Số điện thoại:</label>
-													<input type="number" min="1000" step="1000" class="form-control" id="phone" style="width:40%">
+													<input type="text" class="form-control" id="phone" style="width:40%" name="phone">
 												</div>
 												<div class="form-group">
 													<label for="active">Active</label>
-													<input type="checkbox" id='active' data-toggle="toggle" data-onstyle="success" data-offstyle="danger">
+													<input type="checkbox" id='active' name='active'>
 												</div>
 											</div>
 											<div class="modal-footer">
-												<button type="submit" class="btn btn-info">Submit</button>
+												<button type="submit" class="btn btn-info" name='addSubmit'>Submit</button>
 												<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 											</div>
 										</form>
@@ -85,34 +140,34 @@
 	<!--Sửa sp-->
 	<div id="myModal2" class="modal fade" role="dialog">
 		<div class="modal-dialog">
-
 			<!-- Modal content-->
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 					<h4 class="modal-title">Sửa thông tin nhà cung cấp</h4>
 				</div>
-				<form>
+				<form action="quan-ly-nha-cung-cap.php" method="POST">
 					<div class="modal-body">
 						<div class="form-group">
 							<label for="nameedit">Tên nhà cung cấp:</label>
-							<input type="text" class="form-control" id="nameedit">
+							<input type="text" class="form-control" id="nameedit" name="nameedit">
 						</div>
 						<div class="form-group">
 							<label for="addressedit">Địa chỉ:</label>
-							<textarea rows="6" id="addressedit" class="form-control"></textarea>
+							<textarea rows="6" id="addressedit" name="addressedit" class="form-control"></textarea>
 						</div>
 						<div class="form-group">
 							<label for="phoneedit">Số điện thoại:</label>
-							<input type="number" min="1000" step="1000" class="form-control" id="phoneedit" style="width:40%">
+							<input type="text" class="form-control" id="phoneedit" name="phoneedit" style="width:40%">
 						</div>
 						<div class="form-group">
 							<label for="activeedit">Active</label>
-							<input type="checkbox" id='activeedit' data-toggle="toggle" data-onstyle="success" data-offstyle="danger">
+							<input id='activeedit' name="activeedit" type="checkbox">
 						</div>
 					</div>
 					<div class="modal-footer">
-						<button type="submit" class="btn btn-info">Submit</button>
+						<button type="submit" class="btn btn-danger" name="deleteSubmit" id="deleteSubmit" >Xóa</button>
+						<button type="submit" class="btn btn-info" name="editSubmit" id="editSubmit">Sửa</button>
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 					</div>
 				</form>
@@ -133,6 +188,8 @@
       			showColumns: true,
       			showToggle: true,
       			showRefresh: true,
+            sortName: 'supplier_id',
+            sortOrder: 'asc',
       			toolbar: "#toolbar",
       			pagination: true,
       			uniqueId: "supplier_id",
@@ -150,7 +207,11 @@
       					title: 'Địa Chỉ'
       				},{
                 field: 'is_active',
-                tittle: 'Còn Hoạt Động'
+                title: 'Tình Trạng',
+                formatter: "activeFormatter"
+              },{
+                field: 'supplier_phone_number',
+                title: 'Điện Thoại'
               }
             ],
       			data: suppliers
@@ -169,13 +230,27 @@
       			// 	values: ids
       			// });
       			// $("#remove").prop('disabled', true);
-      			alert("ID muốn xóa là " + ids)
+      			alert("ID muốn xóa là " + ids);
       		});
        });
 
     });
 
-
+      ////YOLO FROM HERE
+    function activeFormatter(value, row, index)
+    {
+      var text = '';
+      if(value == 1)
+      {
+        text = 'Hoạt Động'
+      }else {
+        text = 'Ngưng Hoạt Động'
+      }
+      return [
+        text
+			].join('');
+    };
+      ////YOLO FROM HERE
 		function operateFormatter(value, row, index) {
 			return [
 				'<a class="like" href="#" data-toggle="modal" data-target="#myModal2" onclick=getRow(' + row.supplier_id + ')>' + row.supplier_name,
@@ -186,12 +261,30 @@
 
 		function getIdSelections() {
 			return $.map($("#table1").bootstrapTable('getSelections'), function (row) {
-				return row.supplier_id
+				return row.supplier_id;
 			});
 		}
 
 		function getRow(id) {
 			var row = $("#table1").bootstrapTable('getRowByUniqueId', id)
+      ////Coder code
+      $('#nameedit').val(row.supplier_name);
+      $('#addressedit').val(row.supplier_address);
+      $('#phoneedit').val(row.supplier_phone_number);
+      $('#deleteSubmit').prop( "value", row.supplier_id);
+      $('#editSubmit').prop( "value", row.supplier_id );
+      $('#deleteSubmit').click({name: row.supplier_name}, displayComfirm);
+      // in your function, just grab the event object and go crazy...
+      function displayComfirm(event){
+          confirm('Xác nhận xóa nhà sản xuất '+event.data.name);
+      }
+      if(row.is_active == '1')
+      {
+        $('#activeedit').prop( "checked", true );
+      }else{
+        $('#activeedit').prop( "checked", false );
+      }
+      ////////YOLO FROM HERE
 			//Đổ dữ liệu vào modal2 ở đây
 			// return row
 		}
