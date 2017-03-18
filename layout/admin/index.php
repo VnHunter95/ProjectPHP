616407c428bc7e5f239635ff0b36e2974cebc673
= <tranvytoan@gmail.com>
2017-03-18 11:11:43 +0700

    Add layout admin index to project

--- layout/admin/index.php
+++ layout/admin/index.php
@@ -1,5 +1,3 @@
-HELLO ! THIS IS ADMIN INDEX.php
-       THIS IS A TEST
 <?php
 
   include($_SERVER['DOCUMENT_ROOT'].'/layout/admin/method/getCountUser.php');
@@ -11,31 +9,131 @@ HELLO ! THIS IS ADMIN INDEX.php
   $comment = getCountComment();
   $order = getCountOrder();
 ?>
-</br>
-THIS IS THE USER COUNT: <?php echo $count ?>
-</br>
 
-THIS IS THE COMMENT COUNT: <?php echo $comment ?>
-</br>
-THIS IS THE ORDER COUNT: <?php echo $order ?>
-<?php
-  if(!isset($_SESSION['staff']))
-  {
-    echo '
-          </br>
-          NOT LOGIN
-          </br>
-          RUN LOGIN METHOD WITH USER: Admin Pass: Admin
-          </br>
-          </br> Đã CHạy hàm login, F5 để xem id login và chạy hàm đăng xuất
-        ';
-    login("Admin","Admin");
-  }else{
-    $id = $_SESSION['staff'];
-    echo 'LOGGED IN AS : ID = '.$id;
-    echo '</br>CHAY METHOD LOGOUTADMIN !
-          Đã chạy hàm logoutadmin();
-    ';
-    logoutAdmin();
-  }
-  ?>
+  <?php include('header.php'); ?>
+	<!--/.sidebar-->
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard</h1>
+			</div>
+		</div>
+		<!--/.row-->
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-blue panel-widget ">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked bag">
+								<use xlink:href="#stroked-bag"></use>
+							</svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">120</div>
+							<div class="text-muted">New Orders</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-orange panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked empty-message">
+								<use xlink:href="#stroked-empty-message"></use>
+							</svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">52</div>
+							<div class="text-muted">Comments</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-teal panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked male-user">
+								<use xlink:href="#stroked-male-user"></use>
+							</svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">24</div>
+							<div class="text-muted">New Users</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-red panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked app-window-with-content">
+								<use xlink:href="#stroked-app-window-with-content"></use>
+							</svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large">25.2k</div>
+							<div class="text-muted">Page Views</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/.row-->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Site Traffic Overview</div>
+					<div class="panel-body">
+						<div class="canvas-wrapper">
+							<canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/.row-->
+		<div class="row">
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h4>New Orders</h4>
+						<div class="easypiechart" id="easypiechart-blue" data-percent="92"><span class="percent">92%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h4>Comments</h4>
+						<div class="easypiechart" id="easypiechart-orange" data-percent="65"><span class="percent">65%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h4>New Users</h4>
+						<div class="easypiechart" id="easypiechart-teal" data-percent="56"><span class="percent">56%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+						<h4>Visitors</h4>
+						<div class="easypiechart" id="easypiechart-red" data-percent="27"><span class="percent">27%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/.row-->
+	</div>
+	<!--/.main-->
+  <?php include('footer.php');
