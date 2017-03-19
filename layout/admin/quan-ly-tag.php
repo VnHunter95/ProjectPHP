90b82e1873baa3dbcb195c619c755f837057d592
= <tranvytoan@gmail.com>
2017-03-19 20:20:55 +0700

    Done Admin-Tag

--- layout/admin/quan-ly-tag.php
+++ layout/admin/quan-ly-tag.php
@@ -1,6 +1,28 @@
 
-	<?php include('header.php');?>
-
+	<?php include('header.php');
+		require_once($_SERVER['DOCUMENT_ROOT'].'/class/Tag.Class.php');
+		if(isset($_POST['addSubmit']))
+		{
+			$name = $_POST['name'];
+			$newTag = new Tag(null,$name);
+			if($newTag->save() == 1 )
+			{
+				echo '<script>alert("Thêm Tag Thành Công")</script>';
+			}else {
+				echo '<script>alert("Thêm Tag Thất Bại")</script>';
+			}
+		}else if(isset($_POST['subDelete']))
+		{
+			$id = $_POST['subDelete'];
+			$newTag = new Tag($id,null);
+			if($newTag->delete() == 1 )
+			{
+				echo '<script>alert("Xóa Tag Thành Công")</script>';
+			}else {
+				echo '<script>alert("Xóa Tag Thất Bại")</script>';
+			}
+		}
+	?>
   <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
     			<ol class="breadcrumb">
@@ -13,7 +35,6 @@
     				<h1 class="page-header">Tag</h1>
     			</div>
     </div><!--/.row-->
-
     <div class="row">
     			<div class="col-lg-12">
     				<div class="panel panel-default">
@@ -79,6 +100,7 @@
       <script>
       $(document).ready(function () {
         var url = 'http://'+window.location.hostname+':'+location.port+'/layout/admin/quan-ly-tag/tagData.php';
+				var request;
         $.getJSON(url, function (data) {
             var tags = data;
             // http://bootstrap-table.wenzhixin.net.cn/documentation/
@@ -101,6 +123,7 @@
                 }, {
                   field: 'tag_name',
                   title: 'Tên Tag',
+                  width: '40%',
                   formatter: 'operateFormatter',
                 },{
                   field: 'tag_id',
@@ -135,31 +158,107 @@
       function actionFormatter(value, row, index)
       {
       return['<form action="quan-ly-tag.php" method="POST">'
-            +'<button class="btn btn-danger" name="subDelete" type="button" value="'+value+'" id="btnDelete'+value+'">Xóa</button>'
-            +'<button class="btn btn-primary" type="button" value="'+value+'" id="btnEdit'+value+'" onclick="showButton(\'1\',\''+value+'\')">Sửa</button>'
-            +'<button class="btn btn-primary" name="subEdit" type="button" value="'+value+'" id="btnSave'+value+'" style="display: none;">Lưu</button>'
-            +'<button class="btn btn-secondary" type="button" value="'+value+'" id="btnCancle'+value+'" style="display: none;" onclick="showButton(\'0\',\''+value+'\')">Hủy</button>'
+            +'<button class="btn btn-danger" name="subDelete" type="submit" value="'+value+'" id="btnDelete'+value+'" onclick="return confirmDelete(\''+row.tag_name+'\')">Xóa</button>'
+            +'<button class="btn btn-primary" type="button" value="'+value+'" id="btnEdit'+value+'" onclick="showButton(\'1\',\''+value+'\',\''+row.tag_name+'\')">Sửa</button>'
+            +'<button class="btn btn-primary" name="subEdit" type="button" value="'+value+'" id="btnSave'+value+'" style="display: none;" onclick="edit('+value+')">Lưu</button>'
+            +'<button class="btn btn-secondary" type="button" value="'+value+'" id="btnCancle'+value+'" style="display: none;" onclick="showButton(\'0\',\''+value+'\',\''+row.tag_name+'\')">Hủy</button>'
         +'</form>'].join('');
       }
 
-        function showButton(bool, id)
+			function edit(id)
+			{
+				var newName = $('#nameField'+id).find('#addname').first().val();
+				var urlx = 'http://'+location.hostname+':'+location.port+'/layout/admin/quan-ly-tag/edit.php';
+				//ajax here bois
+				request = $.ajax({
+				url: urlx,
+				data: { "id" : id , "name" : newName},
+				type: "get"
+				});
+
+				// Callback handler that will be called on success
+				request.done(function (response, textStatus, jqXHR){
+						var xmlDoc = response.documentElement;
+						var code = xmlDoc.getElementsByTagName('code')[0].textContent;
+						switch(code)
+						{
+								case '-1':
+													alert("Không thểm chỉnh sửa Tag");
+													break;
+								case '0':
+													alert("Có lỗi xảy ra !");
+													break;
+								case '1':
+													var id =  xmlDoc.getElementsByTagName('id')[0].textContent;
+													var name =  xmlDoc.getElementsByTagName('name')[0].textContent;
+													updateUI(id,name);
+													alert("Chỉnh sửa Tag Thành Công !!!");
+													break;
+								default:
+									alert("Something went wrong");
+								break;
+						}
+				});
+
+				// Callback handler that will be called on failure
+				request.fail(function (jqXHR, textStatus, errorThrown){
+						alert("The following error occurred: "+textStatus);
+				});
+			}
+				function updateUI( tagid , name)
+				{
+					$('#btnEdit'+tagid).click({bool: '1', id: tagid, name: name}, showButton2);
+					$('#btnCancle'+tagid).click({bool: '0', id: tagid, name: name}, showButton2);
+					$('#btnCancle'+tagid).click({name: name}, return confirmDelete2);
+					showButton('0',tagid,name);
+				}
+				function confirmDelete2(event)
+				{
+ 						alert('yolo');
+						return confirm('Xác nhận xóa Tag '+event.data.name);
+				}
+				function showButton2(event)
+				{
+					var bool = event.data.bool;
+					var id   = event.data.id;
+					var name = event.data.name;
+					if(bool == '1')
+					{
+						$('#btnSave'+id).show();
+						$('#btnCancle'+id).show();
+						$('#btnDelete'+id).hide();
+						$('#btnEdit'+id).hide();
+						$('#nameField'+id).html('<input type="text" required maxlength="50" name="name" id="addname" value="'+name+'"/>')
+					}else{
+						$('#btnSave'+id).hide();
+						$('#btnCancle'+id).hide();
+						$('#btnDelete'+id).show();
+						$('#btnEdit'+id).show();
+						$('#nameField'+id).html(name);
+					}
+				}
+        function showButton(bool, id, name)
         {
-          var row = $('#table1').bootstrapTable('getRowByUniqueId', id);
+          //var row = $('#table1').bootstrapTable('getRowByUniqueId', id);
           if(bool == '1')
           {
             $('#btnSave'+id).show();
             $('#btnCancle'+id).show();
             $('#btnDelete'+id).hide();
             $('#btnEdit'+id).hide();
-            $('#nameField'+id).html('<input type="text" required maxlength="50" name="name" id="addname" value="'+row.tag_name+'"/>')
+            $('#nameField'+id).html('<input type="text" required maxlength="50" name="name" id="addname" value="'+name+'"/>')
           }else{
             $('#btnSave'+id).hide();
             $('#btnCancle'+id).hide();
             $('#btnDelete'+id).show();
             $('#btnEdit'+id).show();
-            $('#nameField'+id).html(row.tag_name);
+            $('#nameField'+id).html(name);
           }
-
         }
+
+				function confirmDelete(name)
+				{
+					return confirm('Xác nhận xóa Tag '+name);
+				}
       </script>
 	<?php include('footer.php');?>
