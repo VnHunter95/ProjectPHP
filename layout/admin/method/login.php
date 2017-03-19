f7391f5dd566d134ec5d4bb598f1d7c534bb364b
= <tranvytoan@gmail.com>
2017-03-19 22:38:51 +0700

    Done Login Admin

--- layout/admin/method/login.php
+++ layout/admin/method/login.php
@@ -1,15 +1,17 @@
 <?php
-  session_start();
+  if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+  }
   require_once($_SERVER['DOCUMENT_ROOT'].'/class/Staff.Class.php');
   function login($user,$pass)
   {
     $id = Staff::login($user,$pass);
     if($id != false )
     {
-      echo '<script> alert("Login OK !"); </script>';
-      $_SESSION['staff'] = $id[0]['staff_id'];
+      $_SESSION['staff'] = $id[0];
+      return true;
     }else {
-      echo '<script> alert("CAN\'T LOGIN !!!! !"); </script>';
+      return false;
     }
   }
 ?>
